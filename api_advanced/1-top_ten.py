c1e6293c531626d238cadb84e8c0c435dbef43f9
Christian <p.uhiriwe@alustudent.com>
2025-02-21 19:46:52 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """Prints titles of first 10 hot posts for a given subreddit"""
-    url = "https://www.reddit.com/r/NoStupidQuestions/".format(subreddit)
+    url = "https://www.reddit.com/r/billhsnjansjanasj/".format(subreddit)
     headers = {'User-Agent': 'linux:0:1.0 (by /u/Necessary-Floor-9038)'}
     params = {"limit": 10}
     try:
